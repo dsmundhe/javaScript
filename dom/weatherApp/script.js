17c26ab328addf15b487f92b9d257ae1dce2c9a0
dsmundhe <dipakmundhe2026@gmail.com>
2024-09-17 07:31:00 -0700

    weather app

---
+++ dom/weatherApp/script.js
@@ -0,0 +1,25 @@
+const inName = document.getElementById('city-input');
+const btn = document.getElementById('btn');
+const loading=document.getElementById('loadingShow');
+
+async function getData(params) {
+    loading.innerText='Loading.......'
+    try {
+        const data = await fetch(`http://api.weatherapi.com/v1/current.json?key=6ee045b9cbdb42629c8141825241709%20&q=${inName.value}&aqi=yes`);
+        loading.innerText=''
+
+        return data.json();
+    } catch (error) {
+        loading.innerText=''
+
+        return error.message;
+    }
+
+}
+
+btn.addEventListener('click', async () => {
+     const result = await getData();
+    console.log(result)
+    document.getElementById('cityName').innerText =    `${result.location.name} ${result.location.region} ${result.location.country}`;
+    document.getElementById('cityTemp').innerText=result.current.temp_c;
+})
